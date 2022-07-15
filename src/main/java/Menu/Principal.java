a8f71624f9213594a1aef22e8ec11cd6d358be81
montoyafranco <francomontoya9810@gmail.com>
2022-07-15 17:43:01 -0300

    Structure of menus

---
+++ src/main/java/Menu/Principal.java
@@ -0,0 +1,8 @@
+package Menu;
+
+public class Principal {
+    //Aca basicamente invoco el menu para llamarlo y seteo los defaults
+    //public static void main(String[] args) {
+    //Resources.setDefaults(); algo asi me serviria para poner las cosas por default
+
+}
--- src/main/java/User/User.java
+++ src/main/java/User/User.java
@@ -1,4 +1,22 @@
 package User;
 
 public class User {
+
+    UserType uType;
+    Integer dni;
+    String name;
+    String surname;
+    int age;
+
+    public User(UserType utype, int age , Integer dni, String name, String surname)throws Exception {
+        if(age < 18){
+            throw new Exception("The user most be 18 or older");
+        }
+        this.age = age;
+        this.uType = uType;
+        this.dni = dni;
+        this.name = name;
+        this.surname = surname;
+
+    }
 }
